7fef3b0d829b6293a1dac66aa00b0535d1813da8
Wojciech Oczkowski <wojciech.oczkowski@gmail.com>
2019-11-10 18:43:03 +0100

    add Image Processor stub

--- app/app.py
+++ app/app.py
@@ -1,12 +1,23 @@
 from app.cli import CLI
 from app.input import ImagePreparer
+from app.processing import ImageProcessor
 
 
 class App:
-    def __init__(self, cli: CLI, preparer: ImagePreparer) -> None:
+    def __init__(
+            self,
+            cli: CLI,
+            preparer: ImagePreparer,
+            processor: ImageProcessor,
+    ) -> None:
         self.cli = cli
         self.preparer = preparer
+        self.processor = processor
 
 
-def compose(cli: CLI, preparer: ImagePreparer) -> App:
-    return App(cli=cli, preparer=preparer)
+def compose(
+        cli: CLI,
+        preparer: ImagePreparer,
+        processor: ImageProcessor,
+) -> App:
+    return App(cli=cli, preparer=preparer, processor=processor)
--- tests/app_test.py
+++ tests/app_test.py
@@ -4,13 +4,19 @@ from unittest.mock import Mock
 from app.app import compose, App
 from app.cli import CLI
 from app.input import ImagePreparer
+from app.processing import ImageProcessor
 
 
 class AppComposingTest(TestCase):
     def setUp(self):
         self.cli = Mock(CLI)
         self.preparer = Mock(ImagePreparer)
-        self.app = compose(cli=self.cli, preparer=self.preparer)
+        self.processor = Mock(ImageProcessor)
+        self.app = compose(
+            cli=self.cli,
+            preparer=self.preparer,
+            processor=self.processor,
+        )
 
     def test_compose_app(self):
         self.assertIsInstance(self.app, App)
@@ -20,3 +26,6 @@ class AppComposingTest(TestCase):
 
     def test_has_app_preparer(self):
         self.assertIs(self.app.preparer, self.preparer)
+
+    def test_has_app_processor(self):
+        self.assertIs(self.app.processor, self.processor)
