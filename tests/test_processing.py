2502919f1a76dc132e90a294a03cff360eeb64c2
Wojciech Oczkowski <wojciech.oczkowski@gmail.com>
2019-11-16 21:17:53 +0100

    fix test: adding scale argument to call

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -13,7 +13,7 @@ class ImageProcessorTest(TestCase):
     def test_can_process_image(self):
         image = cv2.imread('./resources/prepared.tif', cv2.IMREAD_UNCHANGED)
         original = cv2.imread('./resources/specimen.tif', cv2.IMREAD_UNCHANGED)
-        actual = self.processor.process(image, original)
+        actual = self.processor.process(image, original, 1)
         expected = cv2.imread('./resources/processed.tif', cv2.IMREAD_UNCHANGED)
         assert_array_equal(expected, actual)
 
