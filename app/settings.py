769eae5fdeb38d73f8da489c2d6d69a26a9bd554
Wojciech Oczkowski <wojciech.oczkowski@gmail.com>
2019-11-30 13:36:59 +0100

    make settings value immutable

--- app/settings.py
+++ app/settings.py
@@ -1,3 +1,7 @@
+def raise_exception(ex: Exception):
+    raise ex
+
+
 class Settings:
     _instance = None
 
@@ -7,9 +11,21 @@ class Settings:
         return cls._instance
 
     def __init__(self, debug_mode=None):
-        if not hasattr(self, 'debug_mode'):
-            self.debug_mode = debug_mode
+        params = {
+            k: v for k, v in locals().items()
+            if k != 'self' and not hasattr(self.__class__, k)
+        }
+        for k, v in params.items():
+            ex = TypeError(f'{k} attribute cannot be assigned')
+            setattr(self, f'_{k}', v)
+            setattr(self.__class__, k, property(
+                lambda o, name=k: getattr(o, f'_{name}'),
+                lambda o, v, name=k: raise_exception(ex),
+            ))
 
     @classmethod
     def clear(cls):
-        Settings._instance = None
+        cls._instance = None
+        props = {k for k, v in cls.__dict__.items() if isinstance(v, property)}
+        for name in props:
+            delattr(cls, name)
--- tests/test_settings.py
+++ tests/test_settings.py
@@ -12,5 +12,11 @@ class SettingsTest(TestCase):
         actual = self.settings()
         self.assertTrue(actual.debug_mode)
 
+    def test_cannot_change_initiated_value(self):
+        self.settings(debug_mode=True)
+        actual = self.settings()
+        with self.assertRaises(TypeError):
+            actual.debug_mode = False
+
     def tearDown(self):
         self.settings.clear()
