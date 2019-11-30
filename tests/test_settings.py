9ab49bc06116e7032beb324e8e238039e15a2393
Wojciech Oczkowski <wojciech.oczkowski@gmail.com>
2019-11-30 14:10:48 +0100

    Fix: add parametrization to settings tests

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -6,17 +6,26 @@ from app.settings import Settings
 class SettingsTest(TestCase):
     def setUp(self):
         self.settings = Settings
+        self.settings_val = {
+            'debug_mode': True,
+        }
 
     def test_can_save_state_globally(self):
-        self.settings(debug_mode=True)
-        actual = self.settings()
-        self.assertTrue(actual.debug_mode)
+        for k, v in self.settings_val.items():
+            with self.subTest(k):
+                self.settings(**{k: v})
+                actual = self.settings()
+                self.assertEqual(getattr(actual, k), v)
+                self.tearDown()
 
     def test_cannot_change_initiated_value(self):
-        self.settings(debug_mode=True)
-        actual = self.settings()
-        with self.assertRaises(TypeError):
-            actual.debug_mode = False
+        for k, v in self.settings_val.items():
+            with self.subTest(k):
+                self.settings(**{k: v})
+                actual = self.settings()
+                with self.assertRaises(TypeError):
+                    setattr(actual, k, v)
+                self.tearDown()
 
     def tearDown(self):
         self.settings.clear()
