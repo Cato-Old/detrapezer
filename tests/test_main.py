d9c6ecaea4ce1ec8dc267ad87eb9c7fa1744bdfe
Wojciech Oczkowski <wojciech.oczkowski@gmail.com>
2019-12-01 13:55:33 +0100

    adding app mock to enhance test performance

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,5 @@
-import os
 from unittest import TestCase
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 
 from app.main import main
 from app.settings import Settings
@@ -12,7 +11,11 @@ class MainEntryPointTest(TestCase):
             'app.main.Settings',
             return_value=Settings(path=r'./resources/specimen.tif'),
         )
+        self.app_mock = Mock()
+        compose_patcher = patch('app.main.compose', return_value=self.app_mock)
+        compose_patcher.start()
         self.addCleanup(Settings.clear)
+        self.addCleanup(compose_patcher.stop)
 
     def test_app_is_closed_when_no_required_args_provided(self):
         with self.assertRaises(SystemExit):
@@ -21,7 +24,7 @@ class MainEntryPointTest(TestCase):
     def test_app_is_running_when_required_args_provided(self):
         with patch('sys.argv', ['_', r'./resources/specimen.tif']):
             main()
-        self.assertTrue(os.path.exists('out.tif'))
+        self.app_mock.run.assert_called_once()
 
     def test_settings_are_initiated_when_app_running(self):
         mock = self.settings_patcher.start()
@@ -31,10 +34,3 @@ class MainEntryPointTest(TestCase):
         mock.assert_called_once_with(
             debug_mode=False, output=None, path='./resources/specimen.tif',
         )
-
-    def tearDown(self):
-        try:
-            os.remove('out.tif')
-        except FileNotFoundError:
-            pass
-
